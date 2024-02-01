7da175b284c59258579c408405c92b9b8055a20d
danh-fptaptech <danhnhts2210026@fpt.edu.vn>
2024-02-01 22:12:21 +0700

    10:12

--- TARS_Delivery/Models/DTOs/req/RDTOBranch.cs
+++ TARS_Delivery/Models/DTOs/req/RDTOBranch.cs
@@ -1,22 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace TARS_Delivery.Models.Entities
+namespace TARS_Delivery.Models.DTOs.req
 {
     public class RDTOBranch
-    {
+    {   
+        [Required(ErrorMessage = "Branch name is required")]
         public string BranchName { get; set; }
         
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid phone number")]
         public string PhoneNumber { get; set; }
-
+        
+        [Required(ErrorMessage = "Address is required")]
         public string Address { get; set; }
+        [Required(ErrorMessage = "Province is required")]
         
         public string Province { get; set; }
+        [Required(ErrorMessage = "City is required")]
         
         public string City { get; set; }
-
+        [Required(ErrorMessage = "District is required")]
+        
         public string District { get; set; }
-
+        [Required(ErrorMessage = "Postal code is required")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Invalid postal code")]
         public int PostalCode { get; set; }
         
     }
--- TARS_Delivery/Models/DtoProfile.cs
+++ TARS_Delivery/Models/DtoProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using TARS_Delivery.Models.DTOs.req;
+using TARS_Delivery.Models.DTOs.res;
 using TARS_Delivery.Models.Entities;
 
 namespace TARS_Delivery.Models;
