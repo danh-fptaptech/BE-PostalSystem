f023dbdffd4ade66824a132b4700956094e95bca
Vu Huu Tai <lkvu92@gmail.com>
2024-02-07 10:27:55 +0700

    Committing unstaged changes before rebase

--- TARS_Delivery/obj/Debug/net8.0/TARS_Delivery.AssemblyInfo.cs
+++ TARS_Delivery/obj/Debug/net8.0/TARS_Delivery.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TARS_Delivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eae1186506ec46da878031bfb9419e2f93102083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1517f3ad161ebef6369ff19974996ce163b1be87")]
 [assembly: System.Reflection.AssemblyProductAttribute("TARS_Delivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TARS_Delivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
