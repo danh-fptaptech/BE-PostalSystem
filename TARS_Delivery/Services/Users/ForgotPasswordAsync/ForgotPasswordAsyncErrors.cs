08c2450a854de20b866686a89e1d62797e871fdb
Long Le <lethaitongg@gmail.com>
2024-02-24 09:55:47 +0700

    merge-23-02

---
+++ TARS_Delivery/Services/Users/ForgotPasswordAsync/ForgotPasswordAsyncErrors.cs
@@ -0,0 +1,8 @@
+﻿using TARS_Delivery.Shared;
+
+namespace TARS_Delivery.Services.Users.ForgotPasswordAsync;
+
+internal static class ForgotPasswordAsyncErrors
+{
+    public static Error NotFound = new(nameof(NotFound), "Not found this user.");
+}
